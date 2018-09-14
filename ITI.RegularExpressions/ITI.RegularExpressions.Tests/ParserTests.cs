bdf4768402547034b14b29e33256b8835fd731cf
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-09-14 17:21:11 +0200

    Enhanced unit tests.

--- ITI.RegularExpressions/ITI.RegularExpressions.Tests/ParserTests.cs
+++ ITI.RegularExpressions/ITI.RegularExpressions.Tests/ParserTests.cs
@@ -5,10 +5,14 @@ namespace ITI.RegularExpressions.Tests
     [TestFixture]
     public class ParserTests
     {
+        [TestCase( "a", "a" )]
+        [TestCase( "ab", "(+ a b)" )]
         [TestCase( "ab|c", "(| (+ a b) c)" )]
         [TestCase( "ab|c*", "(| (+ a b) (* c))" )]
         [TestCase( "a(b|c)*", "(+ a (* (| b c)))" )]
         [TestCase( "(ab|xc)*", "(* (| (+ a b) (+ x c)))" )]
+        [TestCase( "((abc|a*)de*)*fg", "(+ (+ (* (+ (+ (| (+ (+ a b) c) (* a)) d) (* e))) f) g)" )]
+        [TestCase( "((abc|a*)de*)*fg(ab)*", "(+ (+ (+ (* (+ (+ (| (+ (+ a b) c) (* a)) d) (* e))) f) g) (* (+ a b)))" )]
         public void parsing( string regex, string expected )
         {
             Parser p = new Parser( regex );
